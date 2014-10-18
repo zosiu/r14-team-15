2347419ec234c4afabbba94cfc4385665785e2f2
zs <baloghzsof@gmail.com>
2014-10-18 06:04:48 +0200

    Mailer settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,12 +77,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.smtp_settings = {
-  :address        => 'smtp.sendgrid.net',
-  :port           => '587',
-  :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
-}
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.default_url_options = { host: 'stark-everglades-4273.herokuapp.com' }
 end
